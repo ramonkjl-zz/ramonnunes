b542b3834c8f82931b66612bda3eeda0c9939d81
Ramon Nunes <ramonkjl@gmail.com>
2021-08-26 16:14:25 -0300

    CSS modify.

--- src/components/Technology/index.tsx
+++ src/components/Technology/index.tsx
@@ -11,7 +11,13 @@ export const Technology = ({ Icon, title }: Props) => {
   return (
     <TiltComponent options={{ max: 77, scale: 1.3, perspective: 3000 }}>
       <div className={styles.techContainer}>
-        <div className={styles.techCircle}>
+        <div style={{
+          borderRadius: '50px',
+          background: 'linear-gradient(225deg, #cacaca, #f0f0f0)',
+          boxShadow: 'inset -20px 20px 60px #bebebe, inset 20px -20px 60px #ffffff'
+        }}
+          className={styles.techCircle}
+        >
           {Icon}
         </div>
         <h2 className={styles.techHeading}>{title}</h2>
